efb058cd65d0c3fb1b28ccd72096cd15c95cfe7c
Caleb Hughes <c3328308@uon.edu.au>
2019-05-12 03:18:41 +0000

    Task 1 completed.

--- Task_1.c
+++ Task_1.c
@@ -2,50 +2,40 @@
 #include <stdlib.h>
 #include <limits.h>
 
-void pixelMean(float *r, float *g, float *b);
-
 int main() 
 {
   float image[5][5][3];
-  int x, y;
-  //float r, g, b;
+  int x = 0, y = 0;
+  float r, g, b, rTotal, gTotal, bTotal;
   
-  // Don’t change this srand() line!
-  srand(0);
+   //Don’t change this srand() line!
+  srand(0);   
+      
   for(y = 0; y < 2; y++)
   {
     for(x = 0; x < 5; x++) 
     {
-          
+    
       image[x][y][0] = (float)rand()/(float)INT_MAX;
       image[x][y][1] = (float)rand()/(float)INT_MAX;
       image[x][y][2] = (float)rand()/(float)INT_MAX;
       
-      // r = image[x][y][0];
-      //g = image[x][y][1];
-      //b = image[x][y][2];
+      r = image[x][y][0];
+      g = image[x][y][1];
+      b = image[x][y][2];
       
-     pixelMean(&image[x][y][0], &image[x][y][1], &image[x][y][2]);
-     
-    printf("%d %d: %f %f %f\n", x, y, image[x][y][0], image[x][y][1], image[x][y][2]);
-    } 
-  }
-  return 0;
+      rTotal = (r + g + b)/3.0;
+      gTotal = (r + g + b)/3.0;
+      bTotal = (r + g + b)/3.0;
+      
+      r = rTotal;
+      g = gTotal;
+      b = bTotal;
+      
+      printf("%d %d: %f %f %f\n", x, y, r, g, b);
+    }
+ }
+    return 0;
 }
 
-void pixelMean(float *r, float *g, float *b)
-
-{
-    float rMean, gMean, bMean;
-    
-    
-        rMean = (*r+*g+*b)/3.0;
-        gMean = (*r+*g+*b)/3.0;
-        bMean = (*r+*g+*b)/3.0;
-               
-        *r = rMean;
-        *g = gMean;
-        *b = bMean;
-          
-    }
 
